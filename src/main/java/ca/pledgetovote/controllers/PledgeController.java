7866133828f53fd829797c356884f599455383db
Remco de Jongh <rdejongh@obi4wan.com>
2019-02-21 22:44:18 +0100

    Organize import

--- src/main/java/ca/pledgetovote/controllers/PledgeController.java
+++ src/main/java/ca/pledgetovote/controllers/PledgeController.java
@@ -1,9 +1,8 @@
 package ca.pledgetovote.controllers;
 
 import ca.pledgetovote.model.Pledge;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
